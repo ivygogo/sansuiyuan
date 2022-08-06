c87c01ec42d28e600a791b8aac8debbe4e328b3a
Sisol Shie <sisolshie@gmail.com>
2022-08-06 21:46:55 +0800

    add: 產生表格用 若有修改本檔案請暫時別git他 !!!!!!!!!!!)
    之後在做版控的時候請暫時別將這份檔案add !!!!
    (add之後可以在退回)
    不然多人都修改並push上來容易出現conflict!

---
+++ common/src/main/java/tw/edu/ntut/sce/java18/common/EDMTableResetDemo.java
@@ -0,0 +1,30 @@
+package tw.edu.ntut.sce.java18.common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import tw.edu.ntut.sce.java18.common.utils.DBService;
+
+public class EDMTableResetDemo {
+  public static final String UTF8_BOM = "\uFEFF"; // 定義 UTF-8的BOM字元
+
+  public static void main(String args[]) {
+
+    try (
+        Connection connection = DriverManager.getConnection(DBService.getDbTableUrl(),
+            DBService.getUser(), DBService.getPassword());
+        Statement stmt = connection.createStatement();) {
+
+      stmt.executeUpdate(DBService.getDropBooking());
+      System.out.println("表格刪除成功");
+      stmt.executeUpdate(DBService.getCreateBooking());
+      System.out.println("表格產生成功");
+
+    } catch (SQLException e) {
+      System.err.println("新建表格時發生SQL例外: " + e.getMessage());
+      e.printStackTrace();
+    }
+  }
+
+}
