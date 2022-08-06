f21178188a82402ffc9dac2e918fe6a90100608e
ivygogo <ivy.am.office@gmail.com>
2022-08-06 17:13:11 +0800

    第一次丟專案

---
+++ common/src/main/java/tw/edu/ntut/sce/java18/common/SystemInializationLinstener.java
@@ -0,0 +1,34 @@
+package tw.edu.ntut.sce.java18.common;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import tw.edu.ntut.sce.java18.common.utils.GlobalService;
+
+
+
+/**
+ * Application Lifecycle Listener implementation class SystemInializationLinstener
+ *
+ */
+@WebListener
+public class SystemInializationLinstener implements ServletContextListener {
+
+    public SystemInializationLinstener() {
+       
+    }
+
+    public void contextDestroyed(ServletContextEvent sce)  { 
+         
+    }
+
+	
+    public void contextInitialized(ServletContextEvent sce)  { 
+    	GlobalService gs = new GlobalService();
+    	ServletContext sc = sce.getServletContext();
+    	sc.setAttribute("SYSTEM", gs);
+    }
+	
+}
