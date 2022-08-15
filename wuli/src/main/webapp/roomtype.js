cd828cd1a80090119b314473a02e11e0ceab0a8f
Sisol <sisolshie@gmail.com>
2022-08-15 23:07:29 +0800

    重構 格式 & 刪除造成紅x的js
    重構: json /js / html / css的格式

--- wuli/src/main/webapp/roomtype.js
+++ wuli/src/main/webapp/roomtype.js
@@ -1,111 +1,98 @@
 $(function () {
-
-  const roomType0 = {}
-
-  const roomTypeA = {
-    "name": "單人房A",
-    "chair": "椅子*1",
-    "bed": "雙人床",
-    "desk": "大桌子",
-    "sideTable": "小床頭櫃",
-    "wardrobe": "衣櫃",
-    "size": 5,
-    "price": 8100,
-    "balcony": false,
-    "pic": ["SingleA1.jpg", "SingleA2.jpg", "SingleA3.jpg"],
-    "rest": 1,
-    "availableFloor": [1, 3, 8]
-  }
-
-  const roomTypeB = {
-    "name": "單人房B",
-    "chair": "椅子*1",
-    "bed": "雙人床",
-    "desk": "大桌子",
-    "sideTable": "小床頭櫃",
-    "wardrobe": "衣櫃",
-    "size": 5.2,
-    "price": 8200,
-    "balcony": true,
-    "pic": ["SingleB1.jpg", "SingleB2.jpg", "SingleB3.jpg"],
-    "rest": 2,
-    "availableFloor": [1, 5, 8]
-
-  }
-  const roomTypeC = {
-    "name": "單人房C",
-    "chair": "椅子*2",
-    "bed": "單人床",
-    "desk": "大桌子",
-    "sideTable": "小床頭櫃",
-    "wardrobe": "大衣櫃",
-    "size": 5.3,
-    "price": 8300,
-    "balcony": true,
-    "pic": ["SingleC1.jpg", "SingleC2.jpg", "SingleC3.jpg"],
-    "rest": 3,
-    "availableFloor": [1, 2]
-  }
-  const roomTypeD = {
-    "name": "雙人房A",
-    "chair": "椅子*2",
-    "bed": "單人床",
-    "desk": "大桌子",
-    "sideTable": "小床頭櫃",
-    "wardrobe": "衣櫃",
-    "size": 6.1,
-    "price": 8400,
-    "balcony": false,
-    "pic": ["DoubleA1.jpg", "DoubleA2.jpg", "DoubleA3.jpg"],
-    "rest": 4,
-    "availableFloor": [4, 5]
-  }
-  const roomTypeE = {
-    "name": "雙人房B",
-    "chair": "椅子*2",
-    "bed": "單人床",
-    "desk": "大桌子",
-    "sideTable": "小床頭櫃",
-    "wardrobe": "衣櫃",
-    "size": 6.2,
-    "price": 8500,
-    "balcony": true,
-    "pic": ["DoubleB1.jpg", "DoubleB2.jpg", "DoubleB3.jpg"],
-    "rest": 5,
-    "availableFloor": [8]
-  }
-  const roomTypeF = {
-    "name": "雙人房C",
-    "chair": "椅子*2",
-    "bed": "單人床",
-    "desk": "大桌子",
-    "sideTable": "小床頭櫃",
-    "wardrobe": "衣櫃",
-    "size": 6.3,
-    "price": 8600,
-    "balcony": true,
-    "pic": ["DoubleC1.jpg", "DoubleC2.jpg", "DoubleC3.jpg"],
-    "rest": 0,
-    "availableFloor": []
-  }
-
-  //JSon內又是個JSon
-  const roomTypes = {
-    0: roomType0,
-    A: roomTypeA,
-    B: roomTypeB,
-    C: roomTypeC,
-    D: roomTypeD,
-    E: roomTypeE,
-    F: roomTypeF
-  }
+  const roomTypes =
+    {
+      0: {},
+      A: {
+        "name": "單人房A",
+        "chair": "椅子*1",
+        "bed": "雙人床",
+        "desk": "大桌子",
+        "sideTable": "小床頭櫃",
+        "wardrobe": "衣櫃",
+        "size": 5,
+        "price": 8100,
+        "balcony": false,
+        "pic": ["SingleA1.jpg", "SingleA2.jpg", "SingleA3.jpg"],
+        "rest": 1,
+        "availableFloor": [1, 3, 8]
+      },
+      B: {
+        "name": "單人房B",
+        "chair": "椅子*1",
+        "bed": "雙人床",
+        "desk": "大桌子",
+        "sideTable": "小床頭櫃",
+        "wardrobe": "衣櫃",
+        "size": 5.2,
+        "price": 8200,
+        "balcony": true,
+        "pic": ["SingleB1.jpg", "SingleB2.jpg", "SingleB3.jpg"],
+        "rest": 2,
+        "availableFloor": [1, 5, 8]
+      },
+      C: {
+        "name": "單人房C",
+        "chair": "椅子*2",
+        "bed": "單人床",
+        "desk": "大桌子",
+        "sideTable": "小床頭櫃",
+        "wardrobe": "大衣櫃",
+        "size": 5.3,
+        "price": 8300,
+        "balcony": true,
+        "pic": ["SingleC1.jpg", "SingleC2.jpg", "SingleC3.jpg"],
+        "rest": 3,
+        "availableFloor": [1, 2]
+      },
+      D: {
+        "name": "雙人房A",
+        "chair": "椅子*2",
+        "bed": "單人床",
+        "desk": "大桌子",
+        "sideTable": "小床頭櫃",
+        "wardrobe": "衣櫃",
+        "size": 6.1,
+        "price": 8400,
+        "balcony": false,
+        "pic": ["DoubleA1.jpg", "DoubleA2.jpg", "DoubleA3.jpg"],
+        "rest": 4,
+        "availableFloor": [4, 5]
+      },
+      E: {
+        "name": "雙人房B",
+        "chair": "椅子*2",
+        "bed": "單人床",
+        "desk": "大桌子",
+        "sideTable": "小床頭櫃",
+        "wardrobe": "衣櫃",
+        "size": 6.2,
+        "price": 8500,
+        "balcony": true,
+        "pic": ["DoubleB1.jpg", "DoubleB2.jpg", "DoubleB3.jpg"],
+        "rest": 5,
+        "availableFloor": [8]
+      },
+      F: {
+        "name": "雙人房C",
+        "chair": "椅子*2",
+        "bed": "單人床",
+        "desk": "大桌子",
+        "sideTable": "小床頭櫃",
+        "wardrobe": "衣櫃",
+        "size": 6.3,
+        "price": 8600,
+        "balcony": true,
+        "pic": ["DoubleC1.jpg", "DoubleC2.jpg", "DoubleC3.jpg"],
+        "rest": 0,
+        "availableFloor": []
+      }
+    }
 
   $('#room-building').change(function () {
     $('#room-building-value').remove()
     $(document.createElement("div")).text(
       '大樓 : ' + $('#room-building').val()).attr('id',
       'room-building-value').appendTo($('#tenant-information'))
-
   })
 
   $('#room-floor').change(function () {
@@ -116,36 +103,54 @@ $(function () {
   })
 
   $('#room-position').change(function () {
+    // $.ajax("/wuli/RoomTypeServlet").done()
     $('#room-position-value').remove()
     $(document.createElement("div")).text(
       '位置 : ' + $('#room-position').val()).attr('id',
       'room-floor-value').appendTo($('#tenant-information'))
   })
 
+
   $('#room-type').change(function () {
     $('#room-type-value').remove()
+
+    const type = $('#room-type').val()
     $(document.createElement("div")).text(
-      '房型代號 : ' + $('#room-type').val() + '  房型名稱 : ' + $(
+      '房型代號 : ' + type + '  房型名稱 : ' + $(
         '#room-type option:selected').text()).attr('id',
       'room-type-value').appendTo($('#tenant-information'))
 
     $('#room-type-main').removeClass('d-none')
 
-    const roomType = roomTypes[$('#room-type').val()];
+    const roomType = roomTypes[type];
+    console.log(roomType)
+
+    // $.ajax({
+    //   type: "get",
+    //   url: "/wuli/RoomTypeServlet",
+    //   data: "roomType" + [type],
+    //   dataType: "text",
+    //   // roomTypes[$('#room-type').val()]
+    //   success: function () {
+    //     alert(roomType)
+    //     console.log(roomType)
+    //   }
+    // })
 
     if (!roomType) {
       console.error('no room type');
       return;
-    } else if (roomType == roomType0) {
+    } else if (type == 0) {
+      $('#room-type-value').remove()
       $('#room-type-main').addClass('d-none')
       $('#room-price').text("------")
       $('#room-size').text("------")
       $('#room-status').text("------")
+      return;
     }
 
     //清除圖片
     $('#img-slide-block').html("")
-
     //根據房型新增圖片上去
     const renderImg = roomType => {
       const $imgBlock = $('#img-slide-block')
