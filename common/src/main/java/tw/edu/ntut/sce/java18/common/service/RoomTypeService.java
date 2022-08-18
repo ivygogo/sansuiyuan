b2bbfc05f493d61bbb9813207e2893e4b30349bd
Bo-Xuan Fan <coffee0127@gmail.com>
2022-08-18 13:05:37 +0800

    fix: remove unused field and method

--- common/src/main/java/tw/edu/ntut/sce/java18/common/service/RoomTypeService.java
+++ common/src/main/java/tw/edu/ntut/sce/java18/common/service/RoomTypeService.java
@@ -9,14 +9,9 @@ import tw.edu.ntut.sce.java18.common.model.RoomTypeServiceBean;
 public class RoomTypeService {
 
   RoomTypeDao dao;
-  RoomTypeServiceBean roomTypeServiceBean;
 
   public RoomTypeService() {
     dao = new RoomTypeDaoImpl_JDBC();
-  }
-
-  public RoomTypeBean queryRoomType(String type) {
-    return dao.queryRoomType(type);
   }
 
   public Map<String, RoomTypeServiceBean> createRoomTypeObject(String type) {
