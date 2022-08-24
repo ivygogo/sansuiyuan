f688210bc8cd55fdc1578f65f3a209b7a66c073e
Sisol <sisolshie@gmail.com>
2022-08-24 21:22:50 +0800

    新增家具價格的表格內容以及生成的對應SQL指令
    修改受影響的相關roomtype SQL指令

---
+++ data/insertFurniturePrice.sql
@@ -0,0 +1,38 @@
+insert into `furniture_price` (`Id`, `Name`, `Price`)
+values ('1', 'TV', '11000');
+insert into `furniture_price` (`Id`, `Name`, `Price`)
+values ('2', 'Refrigerator', '10500');
+insert into `furniture_price` (`Id`, `Name`, `Price`)
+values ('3', 'AirCondition', NULL);
+insert into `furniture_price` (`Id`, `Name`, `Price`)
+values ('4', 'Heater', NULL);
+insert into `furniture_price` (`Id`, `Name`, `Price`)
+values ('5', 'Mirror', '900');
+insert into `furniture_price` (`Id`, `Name`, `Price`)
+values ('6', 'Flow_Table', '600');
+insert into `furniture_price` (`Id`, `Name`, `Price`)
+values ('7', 'Side_Table_S', '3300');
+insert into `furniture_price` (`Id`, `Name`, `Price`)
+values ('8', 'Side_Table_L', '3500');
+insert into `furniture_price` (`Id`, `Name`, `Price`)
+values ('9', 'Bed_Board_S', '1800');
+insert into `furniture_price` (`Id`, `Name`, `Price`)
+values ('10', 'Bed_Board_L', '2000');
+insert into `furniture_price` (`Id`, `Name`, `Price`)
+values ('11', 'Desk_S', '3400');
+insert into `furniture_price` (`Id`, `Name`, `Price`)
+values ('12', 'Desk_L', '3600');
+insert into `furniture_price` (`Id`, `Name`, `Price`)
+values ('13', 'Bed_S', '3300');
+insert into `furniture_price` (`Id`, `Name`, `Price`)
+values ('14', 'Bed_L', '3500');
+insert into `furniture_price` (`Id`, `Name`, `Price`)
+values ('15', 'Window_Screen_S', '1600');
+insert into `furniture_price` (`Id`, `Name`, `Price`)
+values ('16', 'Window_Screen_L', '2000');
+insert into `furniture_price` (`Id`, `Name`, `Price`)
+values ('17', 'Window_Screen_G', '2000');
+insert into `furniture_price` (`Id`, `Name`, `Price`)
+values ('18', 'Wardrobe', '4500');
+insert into `furniture_price` (`Id`, `Name`, `Price`)
+values ('19', 'Chair', '1800');
