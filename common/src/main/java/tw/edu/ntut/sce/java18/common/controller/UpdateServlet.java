59424950497f09c996568fc1f7839b37afd5836a
Hui <h26211027@gamil.com>
2022-09-18 20:15:55 +0800

    update調整格式

--- common/src/main/java/tw/edu/ntut/sce/java18/common/controller/UpdateServlet.java
+++ common/src/main/java/tw/edu/ntut/sce/java18/common/controller/UpdateServlet.java
@@ -14,7 +14,6 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/booking/UpdateBookServlet")
 public class UpdateServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
-
   /** @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response) */
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
