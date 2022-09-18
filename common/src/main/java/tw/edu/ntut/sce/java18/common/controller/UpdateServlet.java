abc28a51c4742e948fd4c7203798080002cc6dfc
hui <107597815+HuiHui-08@users.noreply.github.com>
2022-09-18 20:38:09 +0800

    Hui/booking (#36)
    * 新增booking後台管理的servlet&jsp+微調前台版面，已完成刪除功能
    
    * no message
    
    * booking的sql指令
    
    * booking sql指令:booker_id 型態改成int 並將欄位位置調到第一個
    
    * 改booker_id型態 String -> Integer
    
    * 刪除功能完成，可以修改日期
    
    * DAO調整格式
    
    * update調整格式
    
    * sql指令太長
    
    * 調整格式
    
    Co-authored-by: Hui <h26211027@gamil.com>

---
+++ common/src/main/java/tw/edu/ntut/sce/java18/common/controller/UpdateServlet.java
@@ -0,0 +1,66 @@
+package tw.edu.ntut.sce.java18.common.controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/booking/UpdateBookServlet")
+public class UpdateServlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    doPost(request, response);
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    String bookerId = request.getParameter("bookerId");
+    String preferTime = request.getParameter("preferTime");
+    System.out.println(bookerId);
+    System.out.println(preferTime);
+    try {
+      // 載入資料庫驅動，註冊到驅動管理器
+      Class.forName("com.mysql.jdbc.Driver");
+      // 資料庫連線字串
+      String url =
+          "jdbc:mysql://127.0.0.1/wulidb?useUnicode=yes&"
+              + "characterEncoding=utf8&useSSL=false&serverTimezone=Asia/"
+              + "Taipei&allowPublicKeyRetrieval=true";
+      // 資料庫使用者名稱
+      String username = "admin";
+      // 資料庫密碼
+      String password = "admin123";
+      // 建立Connection連線
+      Connection conn = DriverManager.getConnection(url, username, password);
+
+      String sql = "update bookingexample set prefer_time=? where booker_id=?";
+      // 獲取PreparedStatement
+      PreparedStatement ps = conn.prepareStatement(sql);
+
+      ps.setString(1, preferTime);
+
+      ps.setString(2, bookerId);
+
+      ps.executeUpdate();
+
+      ps.close();
+
+      conn.close();
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    RequestDispatcher rd = request.getRequestDispatcher("/booking/showBookList.jsp");
+    rd.forward(request, response);
+  }
+}
