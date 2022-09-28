cdea30e400685bc9040849aded98300cebbbf88d
Sisoll <104013838+Sisoll@users.noreply.github.com>
2022-09-28 23:50:44 +0800

    修正聊天室 (#59)
    * 修正訊息的render&一些bug

--- home/src/main/webapp/roomType/roomType.js
+++ home/src/main/webapp/roomType/roomType.js
@@ -1,6 +1,16 @@
 $(function () {
+  //http://localhost:8080/home/roomtype.jsp?type=C
+  const url = location.href
+  let beginType
+  if (url.indexOf('?') != -1) {
+    const ary1 = url.split('?')
+    const ary2 = ary1[1].split('&')
+    const ary3 = ary2[0].split('=')
+    beginType = ary3[1];
+  } else {
+    beginType = 'A'
+  }
 
-  const beginType = "A"  //begin type ,由navBar產生
   const AVAILABLE_ROOM_TYPES = ['A', 'B', 'C', 'D', 'E', 'F']
   let index = AVAILABLE_ROOM_TYPES.indexOf(beginType)
 
@@ -10,20 +20,20 @@ $(function () {
     $.getJSON('/home/common/RoomTypeServlet', type, function (res) {
       const roomType = res;
       $('#room-type-title').text(roomType.name)
-      $('#room-type-price').text(roomType.price + " 元 / 月")
+      $('#room-type-price').text(roomType.price + ' 元 / 月')
       $('#room-type-rest').text(roomType.rest)
       $('#room-type-size').text(roomType.size)
 
       if (roomType.rest === 0) {
-        $('#creatBooking').addClass("invisible")
+        $('#creatBooking').addClass('invisible')
       } else {
-        $('#creatBooking').removeClass("invisible")
+        $('#creatBooking').removeClass('invisible')
       }
 
       if (roomType.availableFloors.length !== 0) {
         $('#room-type-available-floor').text(roomType.availableFloors)
       } else {
-        $('#room-type-available-floor').text("無")
+        $('#room-type-available-floor').text('無')
       }
 
       if (roomType.balcony === true) {
@@ -62,21 +72,21 @@ $(function () {
         let picNum = 0
         return roomType.pics.forEach(roomTypePic => {
             if (picNum === 0) {
-              const $imgSlide = $(document.createElement("div")).addClass(
-                "carousel-item").addClass("active")
-              const $storeImg = $(document.createElement("img")).attr("src",
-                "./images/roomtype/" + roomTypePic).attr("alt",
+              const $imgSlide = $(document.createElement('div')).addClass(
+                'carousel-item').addClass('active')
+              const $storeImg = $(document.createElement('img')).attr('src',
+                './images/roomtype/' + roomTypePic).attr('alt',
                 "don't find pic").addClass(
-                "container-fluid").addClass("img-height")
+                'container-fluid').addClass('img-height')
               $imgSlide.append($storeImg).appendTo($imgBlock)
               picNum++
             } else {
-              const $imgSlide = $(document.createElement("div")).addClass(
-                "carousel-item")
-              const $storeImg = $(document.createElement("img")).attr("src",
-                "./images/roomtype/" + roomTypePic).attr("alt",
+              const $imgSlide = $(document.createElement('div')).addClass(
+                'carousel-item')
+              const $storeImg = $(document.createElement('img')).attr('src',
+                './images/roomtype/' + roomTypePic).attr('alt',
                 "we can't find this picture,check location").addClass(
-                "container-fluid").addClass("img-height").addClass("d-block");
+                'container-fluid').addClass('img-height').addClass('d-block');
               $imgSlide.append($storeImg).appendTo($imgBlock)
             }
           }
@@ -110,12 +120,12 @@ $(function () {
     //todo 確認是否有登入
     $('#move-to-bookingJsp')[0].click()
 
-    if ($('#createBooking').text().includes("取消")) {
-      $('#createBooking').text("我要預約")
+    if ($('#createBooking').text().includes('取消')) {
+      $('#createBooking').text('我要預約')
       $('#formDialogDiv').addClass('d-none')
     } else {
       $('#booking-room-type').val($('#room-type-title')[0].textContent)
-      $('#createBooking').text("取消")
+      $('#createBooking').text('取消')
       $('#formDialogDiv').removeClass('d-none')
     }
 
