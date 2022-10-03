eb9f0daa0e99f707b66d81a4edd1a071cee01379
afrasu <107597942+afrasu@users.noreply.github.com>
2022-10-03 23:54:59 +0800

    修改rent 以下 (#88)
    改成input date 手打輸入檢查太難檢了 怒改！
    狀態為租賃中的房間按產合約會阻擋
    並新增insert contract table 的程式碼在原有的java檔中

--- common/src/main/java/tw/edu/ntut/sce/java18/common/controller/ContractServlet.java
+++ common/src/main/java/tw/edu/ntut/sce/java18/common/controller/ContractServlet.java
@@ -7,6 +7,7 @@ import java.lang.reflect.Type;
 import java.sql.Date;
 import java.sql.SQLException;
 import java.text.ParseException;
+import java.time.LocalDate;
 import java.util.Map;
 import java.util.stream.Collectors;
 import javax.servlet.RequestDispatcher;
@@ -16,6 +17,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import tw.edu.ntut.sce.java18.common.model.ContractBean;
+import tw.edu.ntut.sce.java18.common.model.ContractStatusBean;
 import tw.edu.ntut.sce.java18.common.service.ContractService;
 
 @WebServlet("/rentContractServlet.do")
@@ -41,10 +43,10 @@ public class ContractServlet extends HttpServlet {
     String body = request.getReader().lines().collect(Collectors.joining());
     Type type = new TypeToken<Map<String, String>>() {}.getType();
     Map<String, String> map = new Gson().fromJson(body, type);
-
+    // Tenant table
     java.text.SimpleDateFormat sf = new java.text.SimpleDateFormat("yyyyMMdd");
-    String beginTime = map.get("startY") + map.get("startMM") + map.get("startDD");
-    String endTime = map.get("endY") + map.get("endMM") + map.get("endDD");
+    String beginTime = map.get("startYYYY") + map.get("startMM") + map.get("startDD");
+    String endTime = map.get("endYYYY") + map.get("endMM") + map.get("endDD");
     //    System.out.println("beginTime為:" + beginTime);
     //    System.out.println("endTime為:" + endTime);
 
@@ -68,14 +70,7 @@ public class ContractServlet extends HttpServlet {
     Date end_Time = endTimeSqlDate;
     String room_Number = map.get("roomNumber");
     Integer deposit = Integer.parseInt(map.get("rent"));
-
-    System.out.println(map);
-    System.out.println("Integer member_Id:" + member_Id);
-    System.out.println("String contract_Number:" + contract_Number);
-    System.out.println("Date begin_Time:" + begin_Time);
-    System.out.println("Date end_Time:" + end_Time);
-    System.out.println("String room_Number:" + room_Number);
-    System.out.println("Integer deposit:" + deposit);
+    // System.out.println(map);
 
     ContractBean contract =
         new ContractBean(member_Id, contract_Number, begin_Time, end_Time, room_Number, deposit);
@@ -85,8 +80,63 @@ public class ContractServlet extends HttpServlet {
     } catch (SQLException e) {
       e.printStackTrace();
     }
+    // contract table
+    LocalDate today = LocalDate.now();
+    System.out.println(today);
 
-    RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
+    String roomTypeString = map.get("roomType");
+    if ("A".equals(roomTypeString)) {
+      roomTypeString = "單人房A";
+    } else if ("B".equals(roomTypeString)) {
+      roomTypeString = "單人房B";
+    } else if ("C".equals(roomTypeString)) {
+      roomTypeString = "單人房C";
+    } else if ("D".equals(roomTypeString)) {
+      roomTypeString = "雙人房A";
+    } else if ("E".equals(roomTypeString)) {
+      roomTypeString = "雙人房B";
+    } else if ("F".equals(roomTypeString)) {
+      roomTypeString = "雙人房C";
+    } else {
+      roomTypeString = "請確認房型";
+    }
+    ;
+
+    String status = "租賃中";
+    String name = map.get("renter");
+    String roomNumber = map.get("roomNumber");
+    String roomType = roomTypeString;
+    String paymentStatus = "未繳";
+    String checkFee = "未結算";
+    String checkStatus = "未點交";
+    String pdf = map.get("pdfName");
+    String signedDate = today + "";
+    String depositString = "未退款";
+    Integer hide = 1;
+    Integer memberId = Integer.parseInt(map.get("rentUid"));
+
+    ContractStatusBean contractStatus =
+        new ContractStatusBean(
+            status,
+            name,
+            roomNumber,
+            roomType,
+            paymentStatus,
+            checkFee,
+            checkStatus,
+            pdf,
+            signedDate,
+            depositString,
+            hide,
+            memberId);
+    ContractService service2 = new ContractService();
+    try {
+      service2.insertContract(contractStatus);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    RequestDispatcher dispatcher = request.getRequestDispatcher("/rent.jsp");
     dispatcher.forward(request, response);
   }
 }
--- common/src/main/java/tw/edu/ntut/sce/java18/common/dao/ContractDao.java
+++ common/src/main/java/tw/edu/ntut/sce/java18/common/dao/ContractDao.java
@@ -2,8 +2,11 @@ package tw.edu.ntut.sce.java18.common.dao;
 
 import java.sql.SQLException;
 import tw.edu.ntut.sce.java18.common.model.ContractBean;
+import tw.edu.ntut.sce.java18.common.model.ContractStatusBean;
 
 public interface ContractDao {
 
   ContractBean insertTenant(ContractBean contract) throws SQLException;
+
+  ContractStatusBean insertContract(ContractStatusBean contractStatus) throws SQLException;
 }
